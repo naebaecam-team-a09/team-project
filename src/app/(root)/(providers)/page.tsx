75f90fe3980c3e5d980dd528b7e6fef1a47fd66f
lsy8181 <a4630486@gmail.com>
2024-07-10 10:22:12 +0900

    :sparkles: Feat : 오늘의 날씨ui 완성

--- src/app/(root)/(providers)/page.tsx
+++ src/app/(root)/(providers)/page.tsx
@@ -2,15 +2,20 @@
 
 import { useEffect, useState } from 'react';
 import { getPosts } from '@/services/posts.services';
+import WeatherCard from './_components/WeatherCard';
 
 const Home = () => {
-  const [data, setData] = useState(null);
+  const [data, setData] = useState<{ imageUrl: string; temperature: number } | null>(null);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const result = await getPosts();
-        setData(result);
+        const dummyData = {
+          imageUrl: 'https://via.placeholder.com/100', // 실제 데이터로 수정 필요
+          temperature: 25
+        };
+        setData(dummyData); // 추후 날씨 api 에서 가지고 올꺼임 지금 은 테스트 용으로 더미 데이터 설정
       } catch (error) {
         console.error('에러:', error);
       }
@@ -24,8 +29,8 @@ const Home = () => {
   }
 
   return (
-    <div>
-      <h1>Home Page</h1>
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <WeatherCard imageUrl={data.imageUrl} temperature={data.temperature} />
     </div>
   );
 };
