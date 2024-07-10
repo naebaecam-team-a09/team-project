0f9548af8b9a870d32af761ea78e25c306010f01
lsy8181 <a4630486@gmail.com>
2024-07-10 15:47:28 +0900

    :lipstick: Style : 인기 코디 캐러셀 ui제작

--- src/app/(root)/(providers)/page.tsx
+++ src/app/(root)/(providers)/page.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from 'react';
 import { getPosts } from '@/services/posts.services';
 import WeatherCard from './_components/WeatherCard';
 import RecommendsCard from './_components/RecommendsCard';
+import Popularwear from './_components/PopularWear';
 
 const Home = () => {
   const [data, setData] = useState<{ imageUrl: string; temperature: number } | null>(null);
@@ -14,7 +15,7 @@ const Home = () => {
         const result = await getPosts();
 
         const dummyData = {
-          imageUrl: 'https://via.placeholder.com/100', // 실제 데이터로 수정 필요
+          imageUrl: 'https://via.placeholder.com/800x450', // 실제 데이터로 수정 필요
           temperature: 26
         };
         setData(dummyData); // 추후 날씨 api 에서 가지고 올꺼임 지금 은 테스트 용으로 더미 데이터 설정
@@ -34,6 +35,7 @@ const Home = () => {
     <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
       <WeatherCard imageUrl={data.imageUrl} temperature={data.temperature} />
       <RecommendsCard />
+      <Popularwear />
     </div>
   );
 };
