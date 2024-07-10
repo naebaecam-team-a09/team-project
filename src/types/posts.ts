8d3051de991336d663a964daa2b97c9d572d4bc6
GA HYEON <dlrkxkszz@gmail.com>
2024-07-10 10:18:52 +0900

    :adhesive_bandage:Fix : post 타입 폴더 추가

---
+++ src/types/posts.ts
@@ -0,0 +1,9 @@
+export interface PostType {
+  user_id: string; //로그인, 로그아웃 기능 생기면 빼줄꺼에용
+  image_url: string;
+  category: string[];
+  contents: string;
+  title: string;
+}
+
+export type UpdatedPostType = Partial<PostType>;
