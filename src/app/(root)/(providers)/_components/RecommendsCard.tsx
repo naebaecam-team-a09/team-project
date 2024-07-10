9db7f52e2625ed37692a7fcb27d11ec2c2df03bc
lsy8181 <a4630486@gmail.com>
2024-07-10 11:28:00 +0900

    :lipstick: Style : 옷 추천 ui제작 완료

---
+++ src/app/(root)/(providers)/_components/RecommendsCard.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const RecommendsCard: React.FC = () => {
+  const items = [
+    { id: 1, imageUrl: 'https://via.placeholder.com/150', name: '아이템 1' },
+    { id: 2, imageUrl: 'https://via.placeholder.com/150', name: '아이템 2' },
+    { id: 3, imageUrl: 'https://via.placeholder.com/150', name: '아이템 3' },
+    { id: 4, imageUrl: 'https://via.placeholder.com/150', name: '아이템 4' },
+    { id: 5, imageUrl: 'https://via.placeholder.com/150', name: '아이템 5' },
+    { id: 6, imageUrl: 'https://via.placeholder.com/150', name: '아이템 6' }
+  ];
+
+  return (
+    <div className="flex flex-col items-center ">
+      <h1 className="text-2xl font-bold mb-4">오늘의 날씨에는 이런 옷을 추천 드려요</h1>
+      <h4 className="text-lg mb-8">오늘의 날씨에 대한 짧은 내용 추후 수파베이스에서 코멘트 부분을 가지고 올 꺼임</h4>
+      <div className="grid grid-cols-3 gap-4">
+        {items.map((item) => (
+          <div key={item.id} className="flex flex-col items-center ml-10">
+            <img src={item.imageUrl} alt={item.name} className="w-40 h-40 object-cover rounded-full mb-2" />
+            <p className="text-md font-medium mb-10">{item.name}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default RecommendsCard;
--- src/app/(root)/(providers)/page.tsx
+++ src/app/(root)/(providers)/page.tsx
@@ -3,6 +3,7 @@
 import { useEffect, useState } from 'react';
 import { getPosts } from '@/services/posts.services';
 import WeatherCard from './_components/WeatherCard';
+import RecommendsCard from './_components/RecommendsCard';
 
 const Home = () => {
   const [data, setData] = useState<{ imageUrl: string; temperature: number } | null>(null);
@@ -11,6 +12,7 @@ const Home = () => {
     const fetchData = async () => {
       try {
         const result = await getPosts();
+
         const dummyData = {
           imageUrl: 'https://via.placeholder.com/100', // 실제 데이터로 수정 필요
           temperature: 25
@@ -29,8 +31,11 @@ const Home = () => {
   }
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
       <WeatherCard imageUrl={data.imageUrl} temperature={data.temperature} />
+      <div className="mt-20">
+        <RecommendsCard />
+      </div>
     </div>
   );
 };
