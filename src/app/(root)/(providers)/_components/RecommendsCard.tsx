d81be20d7ab8a19edcd81ff06870bffaaf808a77
lsy8181 <a4630486@gmail.com>
2024-07-10 18:48:48 +0900

    :sparkles: Feat :옷 추천 페이지 수파베이스 연결

--- src/app/(root)/(providers)/_components/RecommendsCard.tsx
+++ src/app/(root)/(providers)/_components/RecommendsCard.tsx
@@ -1,24 +1,56 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getRecommendations } from '@/services/recommendations.services'; // 서비스 파일 경로에 맞게 수정
+import Loading from './Loading';
+
+interface Recommendation {
+  id: number;
+  img_url: string[]; // 수정된 부분
+  contents: string;
+  clothingItems: string[];
+}
 
 const RecommendsCard: React.FC = () => {
-  const items = [
-    { id: 1, imageUrl: 'https://via.placeholder.com/150', name: '아이템 1' },
-    { id: 2, imageUrl: 'https://via.placeholder.com/150', name: '아이템 2' },
-    { id: 3, imageUrl: 'https://via.placeholder.com/150', name: '아이템 3' },
-    { id: 4, imageUrl: 'https://via.placeholder.com/150', name: '아이템 4' },
-    { id: 5, imageUrl: 'https://via.placeholder.com/150', name: '아이템 5' },
-    { id: 6, imageUrl: 'https://via.placeholder.com/150', name: '아이템 6' }
-  ];
+  const [items, setItems] = useState<Recommendation[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await getRecommendations();
+        setItems(data.slice(0, 1)); // 지금은 슬라이스로 짤랐는데 나중에 온도 데이터랑 연결해서 받을 예정
+      } catch (error) {
+        console.error('에러:', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return <Loading />;
+  }
+
+  if (items.length === 0) {
+    return <div>추천 데이터를 불러오지 못했습니다.</div>;
+  }
+
+  const item = items[0];
 
   return (
-    <div className="flex flex-col items-center my-20 ">
+    <div className="flex flex-col items-center my-20">
       <h1 className="text-2xl font-bold mb-4">오늘의 날씨에는 이런 옷을 추천 드려요</h1>
-      <h4 className="text-lg mb-8">오늘의 날씨에 대한 짧은 내용 추후 수파베이스에서 코멘트 부분을 가지고 올 꺼임</h4>
+      <h4 className="text-lg mb-8">{item.contents}</h4>
       <div className="grid grid-cols-3 gap-4">
-        {items.map((item) => (
-          <div key={item.id} className="flex flex-col items-center ml-10">
-            <img src={item.imageUrl} alt={item.name} className="w-40 h-40 object-cover rounded-full mb-2" />
-            <p className="text-md font-medium mb-10">{item.name}</p>
+        {item.clothingItems.map((clothingItem, index) => (
+          <div key={index} className="flex flex-col items-center ml-10">
+            <img
+              src={item.img_url[index]} // 수정된 부분
+              alt={'내용을 불러오는중'}
+              className="w-40 h-40 object-cover rounded-full mb-2"
+            />
+            <p className="text-md font-medium mb-10">{clothingItem}</p>
           </div>
         ))}
       </div>
