eb08ad19acaf8334634850d9bb511c9dece930f7
kwakjaehun <jh5414092@icloud.com>
2024-07-12 02:41:43 +0900

    :sparkles: Feat : 좋아요 관련 함수 작성

--- src/services/likes/likes.service.ts
+++ src/services/likes/likes.service.ts
@@ -1,11 +1,32 @@
 import { createClient } from '@/supabase/client';
 
-export const getLike = async (userId: string) => {
+export const getLike = async ({ userId, postId }: { userId: string; postId: string }) => {
   const supabase = createClient();
-  const { data, error } = await supabase.from('likes').select('*').eq('user_id', userId).single();
+  const { error } = await supabase.from('likes').select('*').eq('user_id', userId).eq('post_id', postId).single();
   if (error) {
-    throw error;
+    return false;
   }
-  console.log(data);
-  return data;
+  return true;
+};
+
+export const toggleLike = async ({ userId, postId, isHeart }: { userId: string; postId: string; isHeart: boolean }) => {
+  const supabase = createClient();
+
+  if (isHeart) {
+    const { data, error } = await supabase.from('likes').delete().eq('user_id', userId).eq('post_id', postId);
+
+    if (error) {
+      throw error;
+    }
+
+    return false;
+  } else {
+    const { data, error } = await supabase.from('likes').upsert([{ user_id: userId, post_id: postId }]);
+
+    if (error) {
+      throw error;
+    }
+
+    return true;
+  }
 };
