2346741dbb3217e9cd7e463a02c747dcfd75ff8e
Ghastly Mouse <gusdnr0839@gmail.com>
2024-07-11 10:46:20 +0900

    Feat : prefetching 추가

--- src/app/(root)/(providers)/(Common)/(posting)/upload/[postId]/page.tsx
+++ src/app/(root)/(providers)/(Common)/(posting)/upload/[postId]/page.tsx
@@ -1,10 +1,22 @@
+import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query';
 import UpdateForm from './_components/UpdateForm';
+import { getPost } from '@/services/posts.services';
 
 const UpdatePage = async ({ params }: { params: { postId: string } }) => {
   const { postId } = params;
+
+  const prefetchQueryClient = new QueryClient();
+
+  await prefetchQueryClient.prefetchQuery({
+    queryKey: ['posts', postId],
+    queryFn: async () => await getPost(postId)
+  });
+
   return (
     <>
-      <UpdateForm postId={postId} />
+      <HydrationBoundary state={dehydrate(prefetchQueryClient)}>
+        <UpdateForm postId={postId} />
+      </HydrationBoundary>
     </>
   );
 };
