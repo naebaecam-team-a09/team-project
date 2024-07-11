127c3759260028aac55fc0a99f83a2cd028f88a0
kwakjaehun <jh5414092@icloud.com>
2024-07-12 00:48:34 +0900

    :sparkles: Feat : commentId용 store 추가

--- src/zustand/store.ts
+++ src/zustand/store.ts
@@ -8,3 +8,12 @@ export const usePostIdStore = create<TpostIdStore>((set) => ({
   postId: '',
   setPostId: (postId) => set(() => ({ postId }))
 }));
+
+interface TcommentIdStore {
+  commentId: string;
+  setCommentId: (commentId: string) => void;
+}
+export const useCommentIdStore = create<TcommentIdStore>((set) => ({
+  commentId: '',
+  setCommentId: (commentId) => set(() => ({ commentId }))
+}));
