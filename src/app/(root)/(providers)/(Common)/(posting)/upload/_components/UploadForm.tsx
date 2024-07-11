b08424ab40d2487caedc5dd63bfbc19270287ae3
GA HYEON <dlrkxkszz@gmail.com>
2024-07-11 18:00:00 +0900

    :lipstick: Style : uploadForm UI 수정완료

--- src/app/(root)/(providers)/(Common)/(posting)/upload/_components/UploadForm.tsx
+++ src/app/(root)/(providers)/(Common)/(posting)/upload/_components/UploadForm.tsx
@@ -3,7 +3,7 @@ import { addPost } from '@/services/posts.services';
 import { createClient } from '@/supabase/client';
 import { PostType } from '@/types/posts';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 
 const UploadForm = () => {
   const [title, setTitle] = useState('');
@@ -16,18 +16,18 @@ const UploadForm = () => {
   const queryClient = useQueryClient();
 
   const categoryList = [
-    '카테고리1',
-    '카테고리2',
-    '카테고리3',
-    '카테고리4',
-    '카테고리5',
-    '카테고리6',
-    '카테고리7',
-    '카테고리8',
-    '카테고리9',
-    '카테고리10',
-    '카테고리11',
-    '카테고리12'
+    '패딩',
+    '두꺼운 코트',
+    '누빔옷',
+    '목도리',
+    '울코트',
+    '히트텍',
+    '가죽 옷',
+    '기모바지',
+    '트렌치 코트',
+    '야상',
+    '점퍼',
+    '스타킹'
   ];
 
   const handleClickCategoryButton = (value: string) => {
@@ -80,63 +80,93 @@ const UploadForm = () => {
     onSuccess: () => queryClient.invalidateQueries({ queryKey: ['posts'] })
   });
 
+  const ref = useRef<HTMLInputElement>(null);
+
+  const imageSelector = () => {
+    if (ref.current) {
+      ref.current!.click();
+    }
+  };
+
   return (
-    <div className="w-full h-full p-40">
-      <h1 className=" p-6 text-6xl text-my-color font-bold border-b-2">게시글 작성</h1>
-      {/* 사진부분 */}
-      <form onSubmit={uploadPost}>
-        <div className="grid grid-cols-2">
-          <div className="flex flex-col p-6">
-            <div className="border-b-2 p-4 ">
-              <h3 className="text-3xl text-my-color font-semibold">상세사진</h3>
+    <div className="w-full flex justify-evenly items-center">
+      <div className="max-w-[1440px] p-20">
+        <h1 className="p-6 text-6xl text-my-color font-bold border-b-2">게시글 작성</h1>
+        {/* 사진부분 */}
+        <form onSubmit={uploadPost} className="mx-auto">
+          <div className="grid grid-cols-2">
+            <div className="flex flex-col p-6 mr-5">
+              <div className="border-b-2 p-4 ">
+                <h3 className="text-3xl text-my-color font-semibold mt-6">상세사진</h3>
+              </div>
+              <div className="flex flex-col items-center">
+                <label className="flex w-full h-[500px] m-10 bg-gray-100 text-my-color font-semibold text-x cursor-pointer">
+                  <input type="file" ref={ref} accept="image/*" onChange={handleSelectImage} className="hidden" />
+                  <img src={previewImage} className="w-full h-[500px]" />
+                </label>
+                <button
+                  type="button"
+                  className="w-2/5 h-full bg-my-color text-white rounded-lg text-md p-2 hover:brightness-90"
+                  onClick={imageSelector}
+                >
+                  {selectedImage ? '이미지 수정' : '이미지 등록'}
+                </button>
+              </div>
             </div>
-            <div className=" h-96 m-10 bg-gray-100">
-              <img src={previewImage} />
-              <input type="file" accept="image/*" onChange={handleSelectImage} />
-              <button className=" text-2xl font-semibold cursor-pointer text-my-color">이미지 선택</button>
+            {/* 작성부분 */}
+            <div className=" flex flex-col p-6 ml-5">
+              <div className="border-b-2 p-4 ">
+                <h3 className="text-3xl text-my-color font-semibold mt-6">이 옷에 대해</h3>
+              </div>
+              <div className="mt-10">
+                <input
+                  type="text"
+                  placeholder="제목"
+                  value={title}
+                  onChange={(e) => setTitle(e.target.value)}
+                  className="text-lg w-full h-auto p-4 mb-8 border-2 rounded-md"
+                />
+                <textarea
+                  placeholder="코디에 대해 설명해주세요!"
+                  value={contents}
+                  onChange={(e) => setContents(e.target.value)}
+                  className="text-lg w-full h-[410px] p-4 border-2 rounded-md"
+                />
+              </div>
             </div>
           </div>
-          {/* 작성부분 */}
-          <div className=" flex flex-col p-6">
-            <div className="border-b-2 p-4 ">
-              <h3 className="text-3xl text-my-color font-semibold">이 옷에 대해</h3>
-            </div>
-            <div className="mt-10">
-              <input
-                type="text"
-                placeholder="title"
-                value={title}
-                onChange={(e) => setTitle(e.target.value)}
-                className="flex text-lg w-full h-auto p-4 mb-4 border-2"
-              />
-              <input
-                type="text"
-                placeholder="코디에 대해 설명해주세요!"
-                value={contents}
-                onChange={(e) => setContents(e.target.value)}
-                className="flex text-lg w-full h-72 p-4  border-2"
-              />
-            </div>
+          {/* 카테고리 */}
+          <div className="flex p-6 border-b-2">
+            <h3 className="text-3xl text-my-color font-semibold mt-6">카테고리</h3>
           </div>
-        </div>
-        {/* 카테고리 */}
-        <div className="flex p-10 border-b-2">
-          <h3 className=" text-3xl text-my-color font-semibold mt-20">카테고리</h3>
-        </div>
-        <div className="grid grid-cols-6 gap-4">
-          {categoryList.map((categoryItem: string) => (
+          <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-6 xl:grid-cols-8 gap-4 place-items-center mt-10">
+            {categoryList.map((categoryItem: string) => (
+              <button
+                key={categoryItem}
+                className={`w-11/12 h-16 bg-gray-100 border-gray-400 border-2 rounded-lg text-my-color hover:brightness-90 ${category.includes(categoryItem) ? 'text-lg bg-gray-500 text-neutral-50' : 'text-lg'}`}
+                type="button"
+                onClick={() => handleClickCategoryButton(categoryItem)}
+              >
+                {categoryItem}
+              </button>
+            ))}
+          </div>
+          <div className="flex justify-end mt-16">
             <button
-              key={categoryItem}
-              className={`w-1/16 bg-my-color rounded-sm text-white hover:brightness-50 ${category.includes(categoryItem) ? 'text-2xl' : 'text-sm'}`}
-              type="button"
-              onClick={() => handleClickCategoryButton(categoryItem)}
+              type="submit"
+              className="w-1/12 h-16 bg-my-color text-white rounded-lg text-xl m-2  hover:brightness-90"
             >
-              {categoryItem}
+              등록
             </button>
-          ))}
-        </div>
-        <button type="submit">등록</button>
-      </form>
+            <button
+              type="submit"
+              className="w-1/12 h-16 bg-red-500 text-white rounded-lg text-xl m-2  hover:brightness-90"
+            >
+              취소
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
