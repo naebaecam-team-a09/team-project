339588d7e47ac99f6a961e21d3e4233cd50c1aa8
lsy8181 <a4630486@gmail.com>
2024-07-12 20:08:12 +0900

    :lipstick: Style : 모달 모션 추가

--- src/components/Modal/CommentCreateModal.tsx
+++ src/components/Modal/CommentCreateModal.tsx
@@ -5,11 +5,13 @@ import { createComment } from '@/services/comments/comments.service';
 import { usePostIdStore } from '@/zustand/store';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { useState } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
 import AlertModal from './AlertModal';
 
 const CommentCreateModal = () => {
   const [contents, setContents] = useState<string>('');
-  const { open, close, isModalOpen } = useModal();
+  const [isClosing, setIsClosing] = useState(false);
+  const { open, close } = useModal();
   const queryClient = useQueryClient();
   const postId = usePostIdStore((state) => state.postId);
   const { mutate } = useMutation({
@@ -30,38 +32,49 @@ const CommentCreateModal = () => {
   };
 
   const handleClickCancelButton = () => {
-    close();
+    setIsClosing(true);
+    setTimeout(() => close(), 300); // 애니메이션 시간과 일치시킴
   };
 
   return (
-    <article className="p-1.5 relative w-[560px] h-[560px] rounded-[36px] bg-white post-modal-shadow opacity-[97%]">
-      <div className="px-2.5 post-modal-background rounded-[28px]">
-        <div className="flex justify-between w-full items-center py-5">
-          <h3 className="ml-4 text-2xl text-[#2d2d2d] font-bold">댓글작성</h3>
-          <button
-            onClick={handleClickCancelButton}
-            className=" text-2xl text-white w-11 h-11 bg-[#6D758F] rounded-full"
-          >
-            X
-          </button>
-        </div>
-        <form className="flex flex-col gap-3" onSubmit={handleSubmit}>
-          <textarea
-            placeholder="댓글을 입력해주세요"
-            className="p-4 resize-none w-full h-[340px] rounded-lg bg-white brightness-110"
-            value={contents}
-            onChange={handleChangeTextarea}
-          />
-          <div className="w-full h-1.5 bg-[#6D758F] rounded-xl"></div>
-          <button
-            className="text-xl text-white font-bold h-14 w-full flex justify-center items-center rounded-xl bg-[#656F8F]"
-            type="submit"
-          >
-            저장하기
-          </button>
-        </form>
-      </div>
-    </article>
+    <AnimatePresence>
+      {!isClosing && (
+        <motion.article
+          initial={{ opacity: 0, scale: 0.8 }}
+          animate={{ opacity: 1, scale: 1 }}
+          exit={{ opacity: 0, scale: 0.8 }}
+          transition={{ duration: 0.3 }}
+          className="p-1.5 relative w-[560px] h-[560px] rounded-[36px] bg-white post-modal-shadow opacity-[97%]"
+        >
+          <div className="px-2.5 post-modal-background rounded-[28px]">
+            <div className="flex justify-between w-full items-center py-5">
+              <h3 className="ml-4 text-2xl text-[#2d2d2d] font-bold">댓글작성</h3>
+              <button
+                onClick={handleClickCancelButton}
+                className=" text-2xl text-white w-11 h-11 bg-[#6D758F] rounded-full"
+              >
+                X
+              </button>
+            </div>
+            <form className="flex flex-col gap-3" onSubmit={handleSubmit}>
+              <textarea
+                placeholder="댓글을 입력해주세요"
+                className="p-4 resize-none w-full h-[340px] rounded-lg bg-white brightness-110"
+                value={contents}
+                onChange={handleChangeTextarea}
+              />
+              <div className="w-full h-1.5 bg-[#6D758F] rounded-xl"></div>
+              <button
+                className="text-xl text-white font-bold h-14 w-full flex justify-center items-center rounded-xl bg-[#656F8F]"
+                type="submit"
+              >
+                저장하기
+              </button>
+            </form>
+          </div>
+        </motion.article>
+      )}
+    </AnimatePresence>
   );
 };
 
