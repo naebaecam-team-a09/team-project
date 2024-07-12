f8764dafff7f6aded8f88f51abe2c20e0f0fa325
lsy8181 <a4630486@gmail.com>
2024-07-12 15:31:38 +0900

    :lipstick: Style :백드랍 그라데이션으로 수정

--- src/components/Modal/BackDrop.tsx
+++ src/components/Modal/BackDrop.tsx
@@ -1,7 +1,11 @@
 import { PropsWithChildren } from 'react';
 
 function Backdrop({ children }: PropsWithChildren) {
-  return <div className=" z-10 fixed inset-0 bg-black/50 flex justify-center items-center">{children}</div>;
+  return (
+    <div className="z-10 fixed inset-0 bg-gradient-to-t from-black/50 to-gray-500/50 flex justify-center items-center">
+      {children}
+    </div>
+  );
 }
 
 export default Backdrop;
