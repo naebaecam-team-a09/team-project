09c0ecb50800d4da95d237bd03906f59078460ee
kwakjaehun <jh5414092@icloud.com>
2024-07-13 03:24:21 +0900

    :sparkles: Feat : DiscoverHeader Comp 추가

---
+++ src/app/(root)/(providers)/(common)/posts/discover/_components/DiscoverHeader/DiscoverHeader.tsx
@@ -0,0 +1,10 @@
+const DiscoverHeader = () => {
+  return (
+    <div className="w-full flex flex-col items-center">
+      <h3 className="text-[36px] text-[#6D758F] font-black">코디 모아보기</h3>
+      <p className="text-lg text-[#6D758F]">여러분들의 예쁜 코디를 봐주세요!</p>
+    </div>
+  );
+};
+
+export default DiscoverHeader;
