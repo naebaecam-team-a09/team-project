d65ffee56186dbeb76b1355353653d5c5f915f4a
kwakjaehun <jh5414092@icloud.com>
2024-07-13 17:32:49 +0900

    save

--- src/services/recommendations/recommendations.service.ts
+++ src/services/recommendations/recommendations.service.ts
@@ -1,5 +1,5 @@
 export const getRecommendations = async () => {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/recommendations`, { method: 'GET' });
+  const response = await fetch(`/api/recommendations`, { method: 'GET' });
   const data = await response.json();
   console.log(data);
   return data;
