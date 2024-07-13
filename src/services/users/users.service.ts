53ec2f1e21c0122dec9b65584827bbfdb0e87d24
kwakjaehun <jh5414092@icloud.com>
2024-07-13 17:19:05 +0900

    save

--- src/services/users/users.service.ts
+++ src/services/users/users.service.ts
@@ -1,25 +1,8 @@
-import { createClient } from '@/supabase/client';
 import { Tables } from '@/types/supabase';
 
 export const getUserInfo = async () => {
-  const supabase = createClient();
-  const {
-    data: { user }
-  } = await supabase.auth.getUser();
-
-  const userId = user?.id;
-
-  if (!userId) return;
-  try {
-    const { data, error } = await supabase.from('users').select('*').eq('id', userId).single();
-
-    if (error) {
-      throw error;
-    }
-
-    return data;
-  } catch (error) {
-    console.log(error);
-  }
+  const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/auth/users`, { method: 'GET' });
+  const data = await response.json();
+  return data;
 };
 export type UserDataType = Tables<'users'>;
