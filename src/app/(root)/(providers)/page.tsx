ec8f1d46827ebe86b2955ea2329c412cffcb49ad
kwakjaehun <jh5414092@icloud.com>
2024-07-13 16:15:08 +0900

    :adhesive_bandage: Fix : 폴더 구조 변경

--- src/app/(root)/(providers)/page.tsx
+++ src/app/(root)/(providers)/page.tsx
@@ -1,10 +1,10 @@
 'use client';
 
-import React, { useRef } from 'react';
-import WeatherCard from './_components/WeatherCard';
-import RecommendsCard from './_components/RecommendsCard';
+import { useRef } from 'react';
 import Popularwear from './_components/PopularWear';
+import RecommendsCard from './_components/RecommendsCard';
 import ScrollToTopButton from './_components/ScrollToTopButton';
+import WeatherCard from './_components/WeatherCard';
 
 const Home = () => {
   const recommendsRef = useRef<HTMLDivElement>(null);
@@ -17,6 +17,7 @@ const Home = () => {
   const handlePopularClick = () => {
     popularwearRef.current?.scrollIntoView({ behavior: 'smooth' });
   };
+  console.log('hello');
 
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
