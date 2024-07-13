377dadb97115f8b1d15cde4e22c68df48f00a77b
kwakjaehun <jh5414092@icloud.com>
2024-07-13 18:34:16 +0900

    :adhesive_bandage: Fix : 모달 수정

--- src/components/Modal/AlertModal.tsx
+++ src/components/Modal/AlertModal.tsx
@@ -1,9 +1,6 @@
 'use client';
-import { useModal } from '@/contexts/modal.context/modal.context';
 
 const AlertModal = ({ content, onNextEvent }: { content: string; onNextEvent: () => void }) => {
-  const { close } = useModal();
-
   return (
     <div className="fixed inset-0 flex items-center justify-center z-50">
       <div className="custom-stroke-container w-[553px] h-[200px] rounded-[20px] bg-gradient-to-t from-[#4E515A] to-[#2B3041] p-6 shadow-md relative">
@@ -16,7 +13,7 @@ const AlertModal = ({ content, onNextEvent }: { content: string; onNextEvent: ()
           <button
             className="bg-white w-[90%] text-black py-2 p-6 px-4 rounded-[14px] shadow"
             onClick={() => {
-              close();
+              onNextEvent();
             }}
           >
             닫기
