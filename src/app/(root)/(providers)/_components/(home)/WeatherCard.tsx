4edb48b428a9bd60fa3939ea9a2be0394f1387e9
lsy8181 <a4630486@gmail.com>
2024-07-15 04:03:15 +0900

    :lipstick: Style : 메인페이지 모션 추가

--- src/app/(root)/(providers)/_components/(home)/WeatherCard.tsx
+++ src/app/(root)/(providers)/_components/(home)/WeatherCard.tsx
@@ -1,5 +1,6 @@
 import { getRecommendations } from '@/services/recommendations/recommendations.service';
 import { useEffect, useState } from 'react';
+import { motion } from 'framer-motion';
 import RecommendsText from './RecommendsText';
 import RecommendsCards from './RecommendsCards';
 
@@ -36,24 +37,64 @@ const WeatherCard = () => {
     fetchWeather();
   }, []);
 
+  const textVariant = {
+    hidden: { opacity: 0, y: 50 },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: { duration: 0.6, delay: 1 }
+    }
+  };
+
   return (
-    <div className="relative w-full h-[650px] ">
+    <div className="relative w-full h-[650px]">
       <img
         src={weatherImgUrl || 'https://via.placeholder.com/800x550'}
         alt="Weather"
         className="w-full h-full object-cover filter blur-sm"
       />
       <div className="absolute top-0 left-0 w-full h-full flex flex-col items-center justify-center bg-black bg-opacity-50 p-4">
-        <h1 className="text-[75px] font-bold mb-2 text-white">현재 온도</h1>
+        <motion.h1
+          className="text-[75px] font-bold mb-2 text-white"
+          initial="hidden"
+          animate="visible"
+          variants={textVariant}
+        >
+          현재 온도
+        </motion.h1>
         <div className="flex items-center mb-2">
-          <div className="h-[3px] w-[117px] bg-[#E6C68F]"></div>
-          <h3 className="text-[48px] font-bold mx-[140px] text-white">
+          <motion.div
+            className="h-[3px] w-[117px] bg-[#E6C68F]"
+            initial="hidden"
+            animate="visible"
+            variants={textVariant}
+          ></motion.div>
+          <motion.h3
+            className="text-[48px] font-bold mx-[140px] text-white"
+            initial="hidden"
+            animate="visible"
+            variants={textVariant}
+          >
             {temperature !== null ? `${temperature}°C` : '로딩 중...'}
-          </h3>
-          <div className="h-[3px] w-[117px] bg-[#E6C68F]"></div>
+          </motion.h3>
+          <motion.div
+            className="h-[3px] w-[117px] bg-[#E6C68F]"
+            initial="hidden"
+            animate="visible"
+            variants={textVariant}
+          ></motion.div>
         </div>
-        <h1 className="text-2xl text-white font-bold mb-4 mt-4">오늘의 날씨에는 이런 옷을 추천 드려요</h1>
-        <RecommendsText />
+        <motion.h1
+          className="text-2xl text-white font-bold mb-4 mt-4"
+          initial="hidden"
+          animate="visible"
+          variants={textVariant}
+        >
+          오늘의 날씨에는 이런 옷을 추천 드려요
+        </motion.h1>
+        <motion.div initial="hidden" animate="visible" variants={textVariant}>
+          <RecommendsText />
+        </motion.div>
       </div>
       <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 translate-y-1/2">
         <RecommendsCards />
--- src/app/(root)/(providers)/_components/ScrollToTopButton.tsx
+++ src/app/(root)/(providers)/_components/ScrollToTopButton.tsx
@@ -24,7 +24,7 @@ const ScrollToTopButton = () => {
 
   return (
     <button
-      className={`fixed bottom-10 right-10 w-20 h-20 bg-gray-500 text-white rounded-full flex items-center justify-center shadow-lg transition-opacity duration-500 transform ${showScrollButton ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-10'}`}
+      className={`fixed bottom-10 right-10 w-20 h-20 bg-[#9F8264] text-white rounded-full flex items-center justify-center shadow-lg transition-opacity duration-500 transform ${showScrollButton ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-10'}`}
       onClick={scrollToTop}
       style={{ transition: 'opacity 0.5s, transform 0.5s' }}
     >
