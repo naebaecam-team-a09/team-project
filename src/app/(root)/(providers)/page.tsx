19fbd3e3105057c5a94e578f420bc3fd55bc1389
lsy8181 <a4630486@gmail.com>
2024-07-15 02:15:02 +0900

    :lipstick: Style : 메인페이지 디자인중

--- src/app/(root)/(providers)/page.tsx
+++ src/app/(root)/(providers)/page.tsx
@@ -1,33 +1,14 @@
 'use client';
 
-import { useRef } from 'react';
-
 import Popularwear from './_components/(home)/PopularWear';
-import RecommendsCard from './_components/(home)/RecommendsCard';
 import WeatherCard from './_components/(home)/WeatherCard';
 import ScrollToTopButton from './_components/ScrollToTopButton';
 
 const Home = () => {
-  const recommendsRef = useRef<HTMLDivElement>(null);
-  const popularwearRef = useRef<HTMLDivElement>(null);
-
-  const handleRecommendClick = () => {
-    recommendsRef.current?.scrollIntoView({ behavior: 'smooth' });
-  };
-
-  const handlePopularClick = () => {
-    popularwearRef.current?.scrollIntoView({ behavior: 'smooth' });
-  };
-
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
-      <WeatherCard onRecommendClick={handleRecommendClick} onPopularClick={handlePopularClick} />
-      <div ref={recommendsRef}>
-        <RecommendsCard />
-      </div>
-      <div ref={popularwearRef}>
-        <Popularwear />
-      </div>
+      <WeatherCard />
+      <Popularwear />
       <ScrollToTopButton />
     </div>
   );
