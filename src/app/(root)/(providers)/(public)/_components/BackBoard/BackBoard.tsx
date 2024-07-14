dbd76a80f7ba4d8c5e933580ff0f67f0185c9f2f
kwakjaehun <jh5414092@icloud.com>
2024-07-15 05:20:38 +0900

    회원가입 배경 수정

--- src/app/(root)/(providers)/(public)/_components/BackBoard/BackBoard.tsx
+++ src/app/(root)/(providers)/(public)/_components/BackBoard/BackBoard.tsx
@@ -2,7 +2,7 @@ const BackBoard = () => {
   return (
     <div className="h-full flex flex-col absolute inset-0">
       <div className="flex-1 bg-[#E7C891]"></div>
-      <div className="flex-1 bg-black"></div>
+      <div className="flex-1 bg-[#13191F]"></div>
     </div>
   );
 };
