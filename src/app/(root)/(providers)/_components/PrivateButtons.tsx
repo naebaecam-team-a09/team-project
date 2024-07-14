384bdaa454e86fe88fe0f05d636c9d4ff913ada3
lsy8181 <a4630486@gmail.com>
2024-07-15 00:49:12 +0900

    :lipstick: Style : 헤더 수정

--- src/app/(root)/(providers)/_components/PrivateButtons.tsx
+++ src/app/(root)/(providers)/_components/PrivateButtons.tsx
@@ -9,10 +9,10 @@ const PrivateButtons = () => {
     <>
       {me && (
         <>
-          <Link href="/posts/write" className="text-gray-700 hover:text-gray-900">
+          <Link href="/posts/write" className="text-gray-100  hover:text-[#E7C891]">
             글작성
           </Link>
-          <Link href="/my-page" className="text-gray-700 hover:text-gray-900">
+          <Link href="/my-page" className="text-gray-100  hover:text-[#E7C891]">
             마이페이지
           </Link>
         </>
