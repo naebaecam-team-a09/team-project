6d4c8a4c65501d10bb94f2646bf175f12529983b
kwakjaehun <jh5414092@icloud.com>
2024-07-15 04:42:46 +0900

    save

--- src/services/weather/weather.service.ts
+++ src/services/weather/weather.service.ts
@@ -1,5 +1,5 @@
 export const getWeather = async () => {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}api/weather`, { method: 'GET' });
+  const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/weather`, { method: 'GET' });
   const data = await response.json();
   return data;
 };
