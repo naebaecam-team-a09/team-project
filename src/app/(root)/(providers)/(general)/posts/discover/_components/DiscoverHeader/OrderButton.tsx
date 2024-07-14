cebf248e54775e8f49cc27a4695e97c0ed49e3d8
kwakjaehun <jh5414092@icloud.com>
2024-07-15 02:57:19 +0900

    둘러보기 페이지 스타일 변경

--- src/app/(root)/(providers)/(general)/posts/discover/_components/DiscoverHeader/OrderButton.tsx
+++ src/app/(root)/(providers)/(general)/posts/discover/_components/DiscoverHeader/OrderButton.tsx
@@ -5,7 +5,7 @@ import { useOrderStore } from '@/zustand/store';
 const OrderButton = ({ content, std }: { content: string; std: OrderType }) => {
   const { order, setOrder } = useOrderStore();
 
-  const buttonStyle = `w-[150px] h-[48px] text-lg font-bold rounded-md shadow-md ${std === order ? 'text-white bg-[#9F8264]' : 'text-[#172E47] bg-[#C8C8C8]'}`;
+  const buttonStyle = `w-[132px] h-[48px] text-lg font-bold rounded-md shadow-md ${std === order ? 'text-white bg-[#9F8264]' : 'text-[#172E47] bg-[#C8C8C8]'}`;
   return (
     <button onClick={() => setOrder(std)} className={buttonStyle}>
       {content}
