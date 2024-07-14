7217edf1cef2f7b40c8b84ab3b31934afd9409b1
lsy8181 <a4630486@gmail.com>
2024-07-15 02:21:18 +0900

    :lipstick: Style : 페이지 컬러 수정

--- src/app/(root)/(providers)/page.tsx
+++ src/app/(root)/(providers)/page.tsx
@@ -6,7 +6,7 @@ import ScrollToTopButton from './_components/ScrollToTopButton';
 
 const Home = () => {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
+    <div className="min-h-screen flex flex-col items-center justify-center ">
       <WeatherCard />
       <Popularwear />
       <ScrollToTopButton />
